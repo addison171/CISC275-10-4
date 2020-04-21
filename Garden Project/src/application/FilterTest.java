afc829e83c89514645e8bbb928c0a06e977080a4
Sohan Gadiraju <55362120+gadirajus13@users.noreply.github.com>
2020-04-21 17:56:59 -0400

    Add files via upload
    Updated

--- Garden Project/src/application/FilterTest.java
+++ Garden Project/src/application/FilterTest.java
@@ -1,5 +1,21 @@
 package application;
 
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Test;
+
 public class FilterTest {
 
+	@Test
+	public void searchTest() {
+		ArrayList<Plant> plants1 = new ArrayList<Plant>();
+		ArrayList<Plant> plants2 = new ArrayList<Plant>();
+		Filter f = new Filter(plants1, "Fall", "Clay", "Sunny" , "Dry");
+		plants1.add(new Plant("Blue Oak", "Clay","Shade", "Wet", "Spring"));
+		plants1.add(new Plant("Red Oak", "Clay","Sunny", "Dry", "Fall"));
+		assertEquals(plants2,f.search(plants1));
+		plants2.add(new Plant("Red Oak", "Clay","Sunny", "Dry", "Fall"));
+		assertEquals(plants2,f.search(plants1));
+	}
+	
 }
--- Garden Project/src/application/Plant.java
+++ Garden Project/src/application/Plant.java
@@ -1,5 +1,82 @@
 package application;
 
-public class Plant {
+import javafx.scene.image.Image;
+
+class Plant {
+	private String name;
+	private String idealSoil;
+	private String idealWaterLevel;
+	private String idealSunlightLevel;
+	private String bloomTime;
+	private Image[] stagesOfPlant;
+	
+	public Plant(String n, String soil,String sun, String water, String bloom){
+		this.name = n;
+		this.idealSoil = soil;
+		this.idealSunlightLevel = sun;
+		this.idealWaterLevel = water;
+		this.bloomTime = bloom;
+		
+	}
+	
+	
+	  // Setter
+	public void setName(String newName) {
+		    this.name = newName;
+	}
+	
+	public String getName() {
+	    return this.name;
+	}
+
+	public void setSoil(String soil) {
+	    this.idealSoil = soil;
+	}
+	
+	public String getSoil() {
+	    return this.idealSoil;
+	}
+	
+	public void setWater(String water) {
+	    this.idealWaterLevel = water;
+	}
+	
+	public String getWater() {
+	    return this.idealWaterLevel;
+	}
+	
+	public void setBloom(String bloom) {
+	    this.bloomTime = bloom;
+	}
+	
+	public String setBloom() {
+	    return this.bloomTime;
+	}
+	
+	public void setNonBloom(Image nonBloom) {
+	    this.stagesOfPlant[0] = nonBloom;
+	}
+	
+	public Image getNonBloom() {
+	    return this.stagesOfPlant[0];
+	}
+	
+	public void setBloom(Image bloom) {
+	    this.stagesOfPlant[1] = bloom;
+	}
+	
+	public Image getBloom() {
+	    return this.stagesOfPlant[1];
+	}
+	
+	public void setSunlight(String sun) {
+	    this.idealSunlightLevel = sun;
+	}
+	
+	public String getSunlight() {
+	    return this.idealSunlightLevel;
+	}
 
 }
+
+

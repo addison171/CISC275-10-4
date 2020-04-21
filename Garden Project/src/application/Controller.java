57f88c399131a4db8b16df33175bcbc1f2cc9235
Sohan Gadiraju <55362120+gadirajus13@users.noreply.github.com>
2020-04-21 19:40:09 -0400

    Controller Version

--- Garden Project/src/application/Controller.java
+++ Garden Project/src/application/Controller.java
@@ -1,17 +1,22 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
  
 public class Controller extends Application {
 	private Model model;
 	private View view;
-	
+	private GardenView gv;
+	private HomeView hv;
+	private InputDataView idv;
+	private InsertObstructionView iov;
+	private Preview pv;
+	private EditCellsView ecv;
+	private SearchAllView searchv;
+	private FinalView fv;
 	
     public static void main(String[] args) {
         launch(args);
@@ -19,20 +24,55 @@ public class Controller extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+      
+    }
+    
+    public EventHandler insertObstructionClick() {
+    	return event -> insertObstructionClicked((MouseEvent) event);
+	}
+    public void insertObstructionClicked(MouseEvent event) {
+    
+    }
+	
+    public EventHandler viewClick() {
+    	return event -> viewClicked((MouseEvent) event);
+	}
+    public void viewClicked(MouseEvent event) {
+    
+    }
+    
+    public EventHandler inputDataClick() {
+    	return event -> inputDataClicked((MouseEvent) event);
+	}
+    public void inputDataClicked(MouseEvent event) {
+    
+    }
+    
+    public EventHandler previewClick() {
+    	return event -> previewClicked((MouseEvent) event);
+	}
+    public void previewClicked(MouseEvent event) {
+    
+    }
+    
+    public EventHandler editCellsClick() {
+    	return event -> editCellsClicked((MouseEvent) event);
+	}
+    public void editCellsClicked(MouseEvent event) {
+    
+    }
+    
+    public EventHandler finalViewClick() {
+    	return event -> finalViewClicked((MouseEvent) event);
+	}
+    public void finalViewClicked(MouseEvent event) {
+    
+    }
+    
+    public EventHandler saveAllClick() {
+    	return event -> saveAllClicked((MouseEvent) event);
+	}
+    public void saveAllClicked(MouseEvent event) {
+    
     }
 }

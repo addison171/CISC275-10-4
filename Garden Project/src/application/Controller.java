aa940142a18a2500722e9ebdae278f8c4e1cddba
Sohan Gadiraju <55362120+gadirajus13@users.noreply.github.com>
2020-04-21 15:47:46 -0400

    Add files via upload

--- Garden Project/src/application/Controller.java
+++ Garden Project/src/application/Controller.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
-public class Main extends Application {
+public class Controller extends Application {
     public static void main(String[] args) {
         launch(args);
     }

1f348c6712594e66e3be8fdf876401cc6bca37ba
Addison Kuykendall <31513571+addison171@users.noreply.github.com>
2020-04-21 19:39:04 -0400

    View changes

--- Garden Project/src/application/View.java
+++ Garden Project/src/application/View.java
@@ -2,6 +2,8 @@ package application;
 
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class View {
@@ -15,6 +17,20 @@ public class View {
 	 * @param theStage
 	 */
 	public View(Stage theStage) {
+		
+	}
+	/**
+	 * Displays the image (usually to display plants)
+	 * @param img - image to be displayed
+	 */
+	public void displayImage(Image img) {
+		
+	}
+	/**
+	 * Adds a button 
+	 * @param b - button being added
+	 */
+	public void addButton(Button b) {
 		
 	}
 }

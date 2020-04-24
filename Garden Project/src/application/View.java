9eb8fe6a9523b418c7f926935f714a3ed1e9a0c5
Addison Kuykendall <31513571+addison171@users.noreply.github.com>
2020-04-24 16:34:34 -0400

    Added features
    Added getWidth, getHeight
    Changed createImage so you pass in the file name and it returns the image

--- Garden Project/src/application/View.java
+++ Garden Project/src/application/View.java
@@ -3,8 +3,10 @@
  */
 package application;
 
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -14,20 +16,34 @@ public class View {
 	Stage theStage;
 	Scene theScene;
 	Plant[][] plants;
+	int canvasWidth = 1380;
+	int canvasHeight = 940;
+    GraphicsContext gc;
 	
 	/**
 	 * 
 	 * @param theStage primary stage being created for the view, an object of stage
 	 */
 	public View(Stage theStage) {
+		theStage.setTitle("Garden");
+		
+		Group root = new Group();
+		theScene = new Scene(root);
+		theStage.setScene(theScene);
+		
+		Canvas canvas = new Canvas(canvasWidth, canvasHeight);
+		root.getChildren().add(canvas);
+		gc = canvas.getGraphicsContext2D();
+		
 		
 	}
 	/**
 	 * Displays the image (usually to display plants)
 	 * @param img - image to be displayed
 	 */
-	public void displayImage(Image img) {
-		
+	public Image createImage(String img) {
+		Image image = new Image(img);
+		return image;
 	}
 	/**
 	 * Adds a button 
@@ -35,5 +51,12 @@ public class View {
 	 */
 	public void addButton(Button b) {
 		
+	}
+	public int getWidth() {
+		return canvasWidth;
+	}
+	
+	public int getHeight() {
+		return canvasHeight;
 	}
 }

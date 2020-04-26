44be11ae9edd486f962996008ef4e9c5c116268a
nicksabatini <sabatini@udel.edu>
2020-04-25 21:14:55 -0400

    some more edits of gardenview

--- Garden Project/src/application/GardenView.java
+++ Garden Project/src/application/GardenView.java
@@ -3,13 +3,19 @@
  */
 package application;
 
+import java.util.ArrayList;
+
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class GardenView {
 			
@@ -26,6 +32,13 @@ public class GardenView {
 	BorderPane borderPane = new BorderPane();
 	
 	Scene scene = new Scene(borderPane);
+	
+	ScrollPane suggestions;
+	VBox suggestionsVb;
+	ArrayList<GridPane> suggestedItems;
+	
+	ScrollPane cart;
+	VBox cartVb;
 
 	public GardenView() {
 		
@@ -34,6 +47,16 @@ public class GardenView {
 		gardenGrid.setPadding(new Insets(10,10,10,10));
 		gardenGrid.setVgap(10);
 		gardenGrid.setHgap(10);
+		gardenGrid.setGridLinesVisible(true);
+		
+		for (int i=0; i<10; i++) {
+			for (int j=0; j<10; j++) {
+				ImageView iv = new ImageView();
+				GridPane.setConstraints(iv, i, j);
+				gardenGrid.getChildren().add(iv);
+			}
+		}
+		
 		//CENTER
 
 		
@@ -72,20 +95,42 @@ public class GardenView {
 		
 		//LEFT
 		
+		
+		//RIGHT
+		
+		this.suggestions = new ScrollPane();
+		this.suggestionsVb = new VBox();
+		this.suggestedItems = new ArrayList<GridPane>();
+		suggestions.setContent(suggestionsVb);
+		
+		Label suggestionsLbl = new Label("Suggestions");
+		
+		suggestionsVb.getChildren().add(suggestionsLbl);
+		
+		//RIGHT
+		
 
 		BorderPane borderpane = new BorderPane();
 		
 		borderpane.setTop(menu);
 		borderpane.setCenter(gardenGrid);
+		borderpane.setRight(suggestions);
 		borderpane.setPadding(new Insets(10,10,10,10));
 
 		this.scene.setRoot(borderpane);
+		
 		
 		
 	}
 
 	static Group GardenRoot = new Group();
 
+	
+	public void addCartItem() {
+		
+	}
+	
+	
 	/**
 	 * updates the screen. Called when something is changed
 	 */

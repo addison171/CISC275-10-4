895e4dd4f5f90c7f02fdbb2468053af95a1cc726
Jonni Tran <Jonni Tran@LAPTOP-E9JDR25P>
2020-04-26 18:47:16 -0400

    Function on Final view

--- Garden Project/src/application/FinalView.java
+++ Garden Project/src/application/FinalView.java
@@ -11,14 +11,84 @@ public class FinalView {
 	Text scores;
 	Text pros;
 	Text cons;
-	Button save;
-	Button Edit;
-	Button newGarden;
+	Button saveButton;
+	Button editButton;
+	Button newgardenButton;
 	static Group finalRoot = new Group();
-
+	
+	
+	
 	/**
 	 * draws the screen
 	 */
+	
+	public FinalView(){
+		
+		BorderPane finalviewBP = new BorderPane();
+		
+		
+		
+		TextArea scores = new TextArea();
+		
+		TextArea pros = new TextArea();
+
+		TextArea cons = new TextArea();
+
+		
+		Label scoresLabel = new Label("Score");
+		Label consLabel = new Label("Cons");
+		Label prosLabel = new Label("Pros");
+		
+	
+		scores.setText("B+");
+		
+		pros.setText("Diverse amount of plants for food");
+		
+		
+		cons.setText("Too much grass");
+		
+		//Buttons within the menu HBox
+				
+		//Edit garden button
+		editButton  = new Button("Edit");
+
+		//Save garden button
+		saveButton = new Button("Save");
+		
+		//New garden button
+		newgardenButton = new Button ("New Design");
+		
+		//Creates the scene
+		
+		//Text goes top of borderpane
+		finalviewBP.setTop(scores);
+		finalviewBP.setTop(pros);
+		finalviewBP.setTop(cons);
+
+		
+		finalviewBP.setMargin(scores,new Insets(2,2,2,2));
+		finalviewBP.setMargin(pros,new Insets(2,2,2,2));
+		finalviewBP.setMargin(cons,new Insets(2,2,2,2));
+		
+		//Buttons at bottom of borderpane
+		finalviewBP.setBottom(editButton);
+		finalviewBP.setBottom(saveButton);
+		finalviewBP.setBottom(newgardenButton);
+		
+		editButton.setPadding(editButton,new Insets(2,2,2,2));
+		saveButton.setPadding(saveButton, new Insets(2,2,2,2));
+		newgardenButton.setPadding(newgardenButton, new Insets(2,2,2,2));
+		
+		
+		
+		Scene finalScene = new Scene(finalviewBP);
+
+		
+	}
+	
+	
+	
+	
 	public void draw() {
 		
 	}

0ff19f37120b4a5b5603821951225174756ec825
Sohan Gadiraju <55362120+gadirajus13@users.noreply.github.com>
2020-04-27 03:29:37 -0400

    Insert Obstruction View

--- Garden Project/src/application/InsertObstructionView.java
+++ Garden Project/src/application/InsertObstructionView.java
@@ -3,24 +3,177 @@
  */
 package application;
 
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.Group;
-import javafx.scene.control.*;
+import java.util.ArrayList;
 
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class InsertObstructionView {
 	Image circle;
 	Image rectangle;
-	ComboBox shapes;
+	Button gardenViewBtn;
+	Button inputDataBtn;
+	Button previewBtn;
+	Button editCellsBtn;
+	Button searchBtn;
+	Button finalViewBtn;
+	Button saveBtn;
+	Button createPlot;
+
+	
+	BorderPane borderpane;
+	Scene scene;
+	
+	ScrollPane cart;
+	
+	GridPane gardenGrid;
+	ComboBox<String> obstruction;
 	TextArea label;
 	static Button add = new Button("add");
 	static Group iovRoot = new Group();
 
 	
 	//updates the view according to what inputs have been changed
+	public InsertObstructionView() {
+		//CENTER
+		gardenGrid = new GridPane();
+		gardenGrid.setPadding(new Insets(10,10,10,10));
+		gardenGrid.setGridLinesVisible(true);
+		gardenGrid.setPrefWidth(550);
+		
+		for (int i=0; i<10; i++) {
+			for (int j=0; j<10; j++) {
+				ImageView iv = new ImageView();
+				iv.setFitHeight(40);
+				iv.setFitWidth(40);
+				gardenGrid.add(iv, i, j);
+			}
+		}
+		//CENTER
+
+		
+		//TOP
+		HBox menu = new HBox();
+		menu.setSpacing(5);
+		
+		//Buttons within the menu HBox
+		//garden view
+		gardenViewBtn = new Button("Garden View");
+		
+		//input data
+		inputDataBtn = new Button("Input Data");
+		
+		//preview
+		previewBtn = new Button("Preview");
+		
+		//edit cells
+		editCellsBtn = new Button("Edit Cells");
+		
+		//search all
+		searchBtn = new Button("Plants Search");
+		
+		//final view 
+		finalViewBtn = new Button("Final View");
+		
+		//save button
+		saveBtn = new Button("Save");
+		
+		menu.getChildren().addAll(inputDataBtn, gardenViewBtn ,previewBtn,editCellsBtn, searchBtn, finalViewBtn,saveBtn);
+		//TOP
+		
+		
+		//LEFT
+		
+		this.cart = new ScrollPane();
+		VBox vb = new VBox();
+		cart.setContent(vb);
+		cart.setPadding(new Insets(10,10,10,10));
+		
+		GridPane gp1 = new GridPane();
+		Label sun1 = new Label("Sunlight preference");
+		GridPane.setConstraints(sun1, 1, 0);
+		Label water1 = new Label("Water preference");
+		GridPane.setConstraints(water1, 1, 1);
+		Label soil1 = new Label("Soil Preference");
+		GridPane.setConstraints(soil1, 1, 2);
+		
+		gp1.getChildren().addAll(sun1,water1,soil1);
+		vb.getChildren().add(gp1);
+		
+		GridPane gp2 = new GridPane();
+		Label sun2 = new Label("Sunlight preference");
+		GridPane.setConstraints(sun2, 1, 0);
+		Label water2 = new Label("Water preference");
+		GridPane.setConstraints(water2, 1, 1);
+		Label soil2 = new Label("Soil Preference");
+		GridPane.setConstraints(soil2, 1, 2);
+		
+		gp2.getChildren().addAll(sun2,water2,soil2);
+		vb.getChildren().add(gp2);
+		
+		
+		//LEFT
+		
+
+		//RIGHT
+		GridPane inputDataBox = new GridPane();
+		inputDataBox.setPadding(new Insets(10,10,10,10));
+		inputDataBox.setVgap(10);
+		inputDataBox.setHgap(10);
+		
+		Text instruct = new Text("Choose the type of obstruction you wish to add");
+		
+		instruct.setWrappingWidth(350);
+		GridPane.setConstraints(instruct, 0, 0, 2, 1 );
+
+		
+		//sunlight label
+		Label obstructionType = new Label("Obstruction");
+		GridPane.setConstraints(obstructionType, 0, 4);
+		
+		//sunlight combo box
+		obstruction = new ComboBox<String>();
+		GridPane.setConstraints(obstruction, 1, 4);
+		obstruction.getItems().addAll(
+				"House",
+				"Pond",
+				"Lake",
+				"Building"
+		);
+		
+		
+		
+		inputDataBox.getChildren().addAll(instruct, obstructionType, obstruction);
+		//inputDataBox.getChildren().addAll(cellFld, cellFld2, sunLbl, sunCbx, waterLbl, waterCbx, soilLbl, soilCbx, saveDataBtn);
+		//RIGHT
+
+
+		//BORDERPANE
+		borderpane = new BorderPane();
+		
+		borderpane.setTop(menu);
+		borderpane.setCenter(gardenGrid);
+		borderpane.setRight(inputDataBox);
+		//borderpane.setLeft(this.cart);
+		borderpane.setPadding(new Insets(10,10,10,10));
+		
+		scene =  new Scene(borderpane);
+		
+		this.scene.setRoot(borderpane);
+		iovRoot.getChildren().add(borderpane);
+
+	}
+	
 	static public void update() {
 	    iovRoot.getChildren().add(add);
 	    add.setTranslateY(.5*940);

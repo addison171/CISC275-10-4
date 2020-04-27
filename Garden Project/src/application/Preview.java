119cd4a985431b6684b67801b33c544a428df774
Sohan Gadiraju <55362120+gadirajus13@users.noreply.github.com>
2020-04-26 21:49:09 -0400

    preview

--- Garden Project/src/application/Preview.java
+++ Garden Project/src/application/Preview.java
@@ -4,17 +4,49 @@
 package application;
 
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 public class Preview {
 
-	ComboBox timeOfDay;
-	ComboBox season;
+	ComboBox<String> timeOfDay;
+	ComboBox<String> season;
 	Button backToEdit;
 	static Group previewRoot = new Group();
+	BorderPane borderpane;
+	Scene scene;
 
-	
+	public Preview() {
+		this.borderpane = new BorderPane();
+		this.scene = new Scene(borderpane);
+
+		
+		//TOP
+		HBox menu = new HBox();
+		menu.setSpacing(5);
+		
+		//Time of Day
+		timeOfDay = new ComboBox<String>();
+		GridPane.setConstraints(timeOfDay, 1, 1);
+		timeOfDay.getItems().addAll(
+				"Morning",
+				"Afternoon",
+				"Night"
+		);
+
+		//Time of Day
+		timeOfDay = new ComboBox<String>();
+		GridPane.setConstraints(timeOfDay, 1, 1);
+		timeOfDay.getItems().addAll(
+				"Morning",
+				"Afternoon",
+				"Night"
+		);
+	}
 	//shows the preview of the garden to the user
 	public void display() {
 	}

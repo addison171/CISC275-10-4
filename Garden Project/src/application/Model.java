696f7ff84d88e544e3f1abcb4d0641c841f730b5
Sohan Gadiraju <55362120+gadirajus13@users.noreply.github.com>
2020-04-26 20:52:22 -0400

    Model

--- Garden Project/src/application/Model.java
+++ Garden Project/src/application/Model.java
@@ -24,6 +24,7 @@ public class Model implements Serializable{
 	private static final long serialVersionUID = 1L;
 	ArrayList<Plant> plants;
 	ArrayList<Plant> allPlants;
+	ArrayList<Plant> cartPlants;
 	Filter f;
 	File saveFile;
 	int score;
